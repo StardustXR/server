d98dc1ee96350bcfc11196d055555779c8b67e8c
Nova <technobaboo@gmail.com>
2022-06-11 23:16:59 -0400

    feat(client+eventloop): get arc reference to event loop in client

--- src/core/client.rs
+++ src/core/client.rs
@@ -1,18 +1,22 @@
+use super::eventloop::EventLoop;
 use super::scenegraph::Scenegraph;
 use crate::nodes::field;
 use crate::nodes::spatial;
 use libstardustxr::messenger::Messenger;
 use mio::net::UnixStream;
 use std::rc::Rc;
+use std::sync::{Arc, Weak};
 
 pub struct Client<'a> {
+	event_loop: Weak<EventLoop>,
 	messenger: Messenger<'a>,
 	scenegraph: Scenegraph<'a>,
 }
 
 impl<'a> Client<'a> {
-	pub fn from_connection(connection: UnixStream) -> Rc<Self> {
+	pub fn from_connection(connection: UnixStream, event_loop_ref: &Arc<EventLoop>) -> Rc<Self> {
 		let client = Rc::new(Client {
+			event_loop: Arc::downgrade(event_loop_ref),
 			messenger: Messenger::new(connection),
 			scenegraph: Default::default(),
 		});
