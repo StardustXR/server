b8e577d5e97c0d576471050e3e88b2577939ca44
Nova <technobaboo@gmail.com>
2022-06-14 20:33:35 -0400

    fix(main): shut clippy up

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,7 @@ fn main() {
 	let event_loop = EventLoop::new(None).expect("Couldn't create server socket");
 	println!("Stardust socket created at {}", event_loop.socket_path);
 
-	loop {
-		match rx.try_recv() {
-			Err(TryRecvError::Empty) => {
-				std::thread::sleep(std::time::Duration::from_millis(1000 / 60))
-			}
-			_ => break,
-		}
+	while let Err(TryRecvError::Empty) = rx.try_recv() {
+		std::thread::sleep(std::time::Duration::from_millis(1000 / 60));
 	}
 }
