ffcb95510846671fd006c1ddcd21536f7abfcca4
Nova <technobaboo@gmail.com>
2022-07-12 22:47:12 -0400

    fix(registry): use dashmap and pointer keys

--- src/core/registry.rs
+++ src/core/registry.rs
@@ -1,8 +1,11 @@
-use parking_lot::Mutex;
-use slab::Slab;
+use std::ptr;
 use std::sync::{Arc, Weak};
 
-pub struct Registry<T>(Mutex<Slab<Weak<T>>>);
+use core::hash::BuildHasherDefault;
+use dashmap::DashMap;
+use rustc_hash::FxHasher;
+
+pub struct Registry<T>(DashMap<usize, Weak<T>, BuildHasherDefault<FxHasher>>);
 
 impl<T> Registry<T> {
 	pub fn add(&self, t: T) -> Arc<T> {
@@ -11,30 +14,22 @@ impl<T> Registry<T> {
 		t_arc
 	}
 	pub fn add_raw(&self, t: &Arc<T>) {
-		self.0.lock().insert(Arc::downgrade(t));
+		self.0
+			.insert(ptr::addr_of!(**t) as usize, Arc::downgrade(t));
 	}
 	pub fn get_valid_contents(&self) -> Vec<Arc<T>> {
 		self.0
-			.lock()
 			.iter()
-			.filter_map(|(_, item)| item.upgrade())
+			.filter_map(|pair| pair.value().upgrade())
 			.collect()
 	}
 	pub fn remove(&self, t: &T) {
-		for item in self.0.lock().iter() {
-			let (idx, item) = item;
-			if let Some(item) = item.upgrade() {
-				if std::ptr::eq(item.as_ref(), t) {
-					self.0.lock().remove(idx);
-					break;
-				}
-			}
-		}
+		self.0.remove(&(ptr::addr_of!(*t) as usize));
 	}
 }
 
 impl<T> Default for Registry<T> {
 	fn default() -> Self {
-		Registry::<T>(Mutex::new(Slab::new()))
+		Registry(DashMap::default())
 	}
 }
