418e9fdc54f1ae0809fcc0ff3d7aad7004e30796
Nova <technobaboo@gmail.com>
2022-11-14 11:53:08 -0500

    fix(wayland): cursor material queue higher

---
+++ src/core/delta.rs
@@ -0,0 +1,43 @@
+use std::ops::{Deref, DerefMut};
+
+pub struct Delta<T> {
+	value: T,
+	changed: bool,
+}
+#[allow(dead_code)]
+impl<T> Delta<T> {
+	pub fn new(value: T) -> Self {
+		Delta {
+			value,
+			changed: false,
+		}
+	}
+	pub fn peek_delta(&self) -> Option<&T> {
+		self.changed.then_some(&self.value)
+	}
+	pub fn delta(&mut self) -> Option<&mut T> {
+		let delta = self.changed.then_some(&mut self.value);
+		self.changed = false;
+		delta
+	}
+	pub fn value(&self) -> &T {
+		&self.value
+	}
+	pub fn value_mut(&mut self) -> &mut T {
+		self.changed = true;
+		&mut self.value
+	}
+}
+impl<T> Deref for Delta<T> {
+	type Target = T;
+
+	fn deref(&self) -> &Self::Target {
+		&self.value
+	}
+}
+impl<T> DerefMut for Delta<T> {
+	fn deref_mut(&mut self) -> &mut Self::Target {
+		self.changed = true;
+		&mut self.value
+	}
+}
