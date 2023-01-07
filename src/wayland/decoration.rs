2f380da62f9c86efe0bdef1e781f85bafd857677
Nova <technobaboo@gmail.com>
2023-01-07 10:15:56 -0500

    refactor(wayland): remove commented out code\

--- src/wayland/decoration.rs
+++ src/wayland/decoration.rs
@@ -20,25 +20,6 @@ use smithay::{
 	wayland::shell::{self, kde::decoration::KdeDecorationHandler},
 };
 
-// impl XdgDecorationHandler for WaylandState {
-// 	fn new_decoration(&mut self, toplevel: smithay::wayland::shell::xdg::ToplevelSurface) {
-// 		toplevel.with_pending_state(|state| {
-// 			state.decoration_mode = Some(Mode::ServerSide);
-// 		});
-// 		toplevel.send_configure();
-// 	}
-
-// 	fn request_mode(
-// 		&mut self,
-// 		_toplevel: smithay::wayland::shell::xdg::ToplevelSurface,
-// 		_mode: smithay::reexports::wayland_protocols::xdg::decoration::zv1::server::zxdg_toplevel_decoration_v1::Mode,
-// 	) {
-// 	}
-
-// 	fn unset_mode(&mut self, _toplevel: smithay::wayland::shell::xdg::ToplevelSurface) {}
-// }
-// delegate_xdg_decoration!(WaylandState);
-
 impl GlobalDispatch<ZxdgDecorationManagerV1, (), WaylandState> for WaylandState {
 	fn bind(
 		_state: &mut WaylandState,
