4204cfae8817a111e65d9653c1550b988f421f68
Nova <technobaboo@gmail.com>
2025-03-02 21:30:56 -0800

    feat: dmabuf initial impl

--- src/wayland/dmabuf/buffer_backing.rs
+++ src/wayland/dmabuf/buffer_backing.rs
@@ -1,9 +1,25 @@
 use super::buffer_params::BufferParams;
+use crate::wayland::GraphicsInfo;
+use khronos_egl::{self as egl, ClientBuffer};
 use mint::Vector2;
-use parking_lot::Mutex;
-use std::sync::Arc;
+use std::{
+	os::fd::AsRawFd,
+	sync::{Arc, OnceLock},
+};
 use stereokit_rust::tex::{Tex, TexFormat, TexType};
 use waynest::server::protocol::stable::linux_dmabuf_v1::zwp_linux_buffer_params_v1::Flags;
+
+// EGL extension constants for DMA-BUF
+const EGL_WIDTH: i32 = 0x3057;
+const EGL_HEIGHT: i32 = 0x3056;
+const EGL_LINUX_DRM_FOURCC_EXT: i32 = 0x3272;
+const EGL_DMA_BUF_PLANE0_FD_EXT: i32 = 0x3272;
+const EGL_DMA_BUF_PLANE0_OFFSET_EXT: i32 = 0x3273;
+const EGL_DMA_BUF_PLANE0_PITCH_EXT: i32 = 0x3274;
+const EGL_DMA_BUF_PLANE0_MODIFIER_LO_EXT: i32 = 0x3443;
+const EGL_DMA_BUF_PLANE0_MODIFIER_HI_EXT: i32 = 0x3444;
+const EGL_LINUX_DMA_BUF_EXT: i32 = 0x3270;
+const EGL_NO_BUFFER: *mut std::ffi::c_void = std::ptr::null_mut();
 
 /// Parameters for a shared memory buffer
 #[derive(Debug)]
@@ -11,46 +27,91 @@ pub struct DmabufBacking {
 	params: Arc<BufferParams>,
 	size: Vector2<usize>,
 	format: u32,
-	flags: Flags,
-	tex: Mutex<Tex>,
+	_flags: Flags,
+	tex: OnceLock<Tex>,
 }
 
 impl DmabufBacking {
 	pub fn new(params: Arc<BufferParams>, size: Vector2<usize>, format: u32, flags: Flags) -> Self {
-		let tex = Tex::new(
-			TexType::ImageNomips | TexType::Dynamic,
-			TexFormat::RGBA32,
-			nanoid::nanoid!(),
-		);
-
 		Self {
 			params,
 			size,
 			format,
-			flags,
-			tex: Mutex::new(tex),
+			_flags: flags,
+			tex: OnceLock::new(),
 		}
 	}
 
-	pub fn update_tex(&self) -> Option<Tex> {
-		// TODO: Implement DMA-BUF texture update using EGL/Vulkan
-		// This requires:
-		// 1. Import DMA-BUF into GPU texture using EGL/Vulkan
-		// 2. Handle multi-plane formats
-		// 3. Apply format/modifier transformations
-		//
-		// For now, we can access the DMA-BUF parameters like this:
-		// let format = params.get_format();
-		// let flags = params.get_flags();
-		// for plane_idx in 0.. {
-		//     if let Some(plane) = params.get_plane(plane_idx) {
-		//         // Use plane.fd, plane.offset, plane.stride, plane.modifier
-		//         // to import the DMA-BUF into a GPU texture
-		//     } else {
-		//         break;
-		//     }
-		// }
-		None
+	pub fn update_tex(&self, graphics_info: &GraphicsInfo) -> Option<Tex> {
+		let tex_ref = self.tex.get_or_init(|| {
+			let mut tex = Tex::new(
+				TexType::ImageNomips | TexType::Dynamic,
+				TexFormat::RGBA32,
+				nanoid::nanoid!(),
+			);
+
+			// Get plane info from params
+			let planes = self.params.lock_planes();
+			if let Some(plane) = planes.get(&0) {
+				// Create EGL image
+				let image = graphics_info.egl_instance.create_image(
+					graphics_info.display,
+					graphics_info.context,
+					EGL_LINUX_DMA_BUF_EXT as u32,
+					unsafe { ClientBuffer::from_ptr(EGL_NO_BUFFER) },
+					&[
+						EGL_WIDTH as usize,
+						self.size.x as usize,
+						EGL_HEIGHT as usize,
+						self.size.y as usize,
+						EGL_LINUX_DRM_FOURCC_EXT as usize,
+						self.format as usize,
+						EGL_DMA_BUF_PLANE0_FD_EXT as usize,
+						plane.fd.as_raw_fd() as usize,
+						EGL_DMA_BUF_PLANE0_OFFSET_EXT as usize,
+						plane.offset as usize,
+						EGL_DMA_BUF_PLANE0_PITCH_EXT as usize,
+						plane.stride as usize,
+						EGL_DMA_BUF_PLANE0_MODIFIER_LO_EXT as usize,
+						0,
+						EGL_DMA_BUF_PLANE0_MODIFIER_HI_EXT as usize,
+						0,
+						egl::ATTRIB_NONE,
+						0,
+					],
+				);
+
+				if let Ok(image) = image {
+					// Create and bind GL texture
+					let mut gl_tex = 0;
+					unsafe {
+						gl::GenTextures(1, &mut gl_tex);
+						gl::BindTexture(gl::TEXTURE_2D, gl_tex);
+					}
+
+					// Set the native texture handle directly
+					// Mesa will handle the OES texture implicitly
+					tex.set_native_surface(
+						gl_tex as *mut std::os::raw::c_void,
+						TexType::ImageNomips | TexType::Dynamic,
+						0x8058, // GL_RGBA8
+						self.size.x as i32,
+						self.size.y as i32,
+						1,    // single surface
+						true, // we own this texture
+					);
+
+					// Clean up EGL image
+					graphics_info
+						.egl_instance
+						.destroy_image(graphics_info.display, image)
+						.ok();
+				}
+			}
+
+			tex
+		});
+		Tex::find(tex_ref.get_id()).ok()
 	}
 
 	pub fn size(&self) -> Vector2<usize> {
