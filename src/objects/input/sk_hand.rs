e7ac09589ebd9e2805289f6a36b2fb91e3fc44c4
Nova <technobaboo@gmail.com>
2025-04-19 01:32:35 -0700

    fix(input): don't limit to closest handler

--- src/objects/input/sk_hand.rs
+++ src/objects/input/sk_hand.rs
@@ -179,7 +179,8 @@ impl SkHand {
 		}
 
 		let sorted_handlers = get_sorted_handlers(&self.input, distance_calculator);
-		self.input.set_handler_order(sorted_handlers.iter());
+		self.input
+			.set_handler_order(sorted_handlers.iter().map(|(handler, _)| handler));
 	}
 }
 impl Drop for SkHand {
