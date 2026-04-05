3bd364c494bc2ba4ee244ad997f8e2e8d1bd2094
Schmarni <marnistromer@gmail.com>
2026-04-05 10:30:03 +0200

    Revert "feat: half broken selfref impl"
    This reverts commit ad92f217f60eed358d588c108a0d8364b17e21a0.
    We need a shared inner as else we cannot ensure that the shared data is
    alive without causing an Arc loop, causing a memory leak.

--- foundation/src/lib.rs
+++ foundation/src/lib.rs
@@ -1,11 +1,9 @@
 pub mod delta;
 pub mod error;
 mod id;
-pub mod on_drop;
 pub mod registry;
 pub mod resource;
-pub mod selfref;
 pub mod task;
-pub use selfref::SelfRef;
+pub mod on_drop;
 
 pub use id::*;
