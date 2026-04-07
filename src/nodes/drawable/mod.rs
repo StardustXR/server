03d562f71ab0a6133c0337d9a00416b6bc26c76f
Schmarni <marnistromer@gmail.com>
2026-04-07 19:54:40 +0200

    fix: make dmatex fully compile
    Signed-off-by: Schmarni <marnistromer@gmail.com>

--- src/nodes/drawable/mod.rs
+++ src/nodes/drawable/mod.rs
@@ -1,5 +1,8 @@
-// pub mod dmatex;
+pub mod dmatex;
 pub mod lines;
 // pub mod model;
 pub mod sky;
 // pub mod text;
+
+#[derive(bevy::ecs::schedule::SystemSet, Hash, Debug, PartialEq, Eq, Clone, Copy)]
+pub struct ModelNodeSystemSet;
