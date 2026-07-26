eaefdba9cfcd10a4b6f33bbc84c61a33acfe2b3b
Nova <technobaboo@gmail.com>
2026-07-25 21:09:58 -0700

    refactor(registry): use rwlock instead of cloning

--- foundation/src/registry.rs
+++ foundation/src/registry.rs
@@ -1,17 +1,16 @@
 #![allow(dead_code)]
 
-use dashmap::DashMap;
-use parking_lot::{MappedMutexGuard, Mutex, MutexGuard, const_mutex};
+use parking_lot::{MappedMutexGuard, Mutex, MutexGuard, RwLock, const_mutex};
 use rustc_hash::FxHashMap;
 use std::ops::Deref;
 use std::ptr;
 use std::sync::{Arc, LazyLock, Weak};
 
 #[derive(Debug)]
-pub struct Registry<T: Send + Sync + ?Sized>(MaybeLazy<DashMap<usize, Weak<T>>>);
+pub struct Registry<T: Send + Sync + ?Sized>(MaybeLazy<RwLock<FxHashMap<usize, Weak<T>>>>);
 impl<T: Send + Sync + ?Sized> Registry<T> {
 	pub const fn new() -> Self {
-		Registry(MaybeLazy::Lazy(LazyLock::new(DashMap::default)))
+		Registry(MaybeLazy::Lazy(LazyLock::new(Default::default)))
 	}
 	pub fn add(&self, t: T) -> Arc<T>
 	where
@@ -23,28 +22,28 @@ impl<T: Send + Sync + ?Sized> Registry<T> {
 	}
 	pub fn add_raw(&self, t: &Arc<T>) {
 		self.0
+			.write()
 			.insert(Arc::as_ptr(t) as *const () as usize, Arc::downgrade(t));
 	}
 	pub fn contains(&self, t: &T) -> bool {
 		self.0
+			.read()
 			.contains_key(&(ptr::addr_of!(*t) as *const () as usize))
 	}
 	pub fn get_changes(old: &Registry<T>, new: &Registry<T>) -> (Vec<Arc<T>>, Vec<Arc<T>>) {
 		let mut added = Vec::new();
 		let mut removed = Vec::new();
 
-		for pair in new.0.iter() {
-			let (id, entry) = pair.pair();
+		for (id, entry) in new.0.read().iter() {
 			if let Some(entry) = entry.upgrade()
-				&& !old.0.contains_key(id)
+				&& !old.0.read().contains_key(id)
 			{
 				added.push(entry);
 			}
 		}
-		for pair in old.0.iter() {
-			let (id, entry) = pair.pair();
+		for (id, entry) in old.0.read().iter() {
 			if let Some(entry) = entry.upgrade()
-				&& !new.0.contains_key(id)
+				&& !new.0.read().contains_key(id)
 			{
 				removed.push(entry);
 			}
@@ -53,25 +52,19 @@ impl<T: Send + Sync + ?Sized> Registry<T> {
 	}
 	pub fn get_valid_contents(&self) -> Vec<Arc<T>> {
 		// TODO: optimize so this happens with only one iter, when the upgrade fails
-		self.0.retain(|_, v| v.strong_count() > 0);
-		self.0
-			.iter()
-			.filter_map(|pair| pair.value().upgrade())
-			.collect()
+		self.0.write().retain(|_, v| v.strong_count() > 0);
+		self.0.read().values().filter_map(|v| v.upgrade()).collect()
 	}
 	pub fn set(&self, other: &Registry<T>) {
-		self.clear();
-		for (key, value) in other.0.deref().clone().into_iter() {
-			self.0.insert(key, value);
-		}
+		*self.0.write() = other.0.read().clone();
 	}
 	pub fn take_valid_contents(&self) -> Vec<Arc<T>> {
 		let contents = self.get_valid_contents();
-		self.0.clear();
+		self.clear();
 		contents
 	}
 	pub fn retain<F: Fn(&Arc<T>) -> bool>(&self, f: F) {
-		self.0.retain(|_, v| {
+		self.0.write().retain(|_, v| {
 			let Some(v) = v.upgrade() else {
 				// why would we want to retain things we can't upgrade?
 				return true;
@@ -80,21 +73,24 @@ impl<T: Send + Sync + ?Sized> Registry<T> {
 		})
 	}
 	pub fn remove(&self, t: &T) {
-		self.0.remove(&(ptr::addr_of!(*t) as *const () as usize));
+		self.0
+			.write()
+			.remove(&(ptr::addr_of!(*t) as *const () as usize));
 	}
 	pub fn clear(&self) {
-		self.0.clear();
+		self.0.write().clear();
 	}
 	pub fn is_empty(&self) -> bool {
-		if self.0.is_empty() {
+		let map = self.0.read();
+		if map.is_empty() {
 			return true;
 		}
-		self.0.iter().all(|v| v.value().strong_count() == 0)
+		map.values().all(|v| v.strong_count() == 0)
 	}
 }
 impl<T: Send + Sync + ?Sized> Clone for Registry<T> {
 	fn clone(&self) -> Self {
-		Self(self.0.clone())
+		Self(MaybeLazy::NonLazy(RwLock::new(self.0.read().clone())))
 	}
 }
 impl<T: Send + Sync + ?Sized> Default for Registry<T> {
@@ -104,11 +100,11 @@ impl<T: Send + Sync + ?Sized> Default for Registry<T> {
 }
 impl<T: Send + Sync + Sized> FromIterator<Arc<T>> for Registry<T> {
 	fn from_iter<I: IntoIterator<Item = Arc<T>>>(iter: I) -> Self {
-		Registry(MaybeLazy::NonLazy(
+		Registry(MaybeLazy::NonLazy(RwLock::new(
 			iter.into_iter()
 				.map(|i| (Arc::as_ptr(&i) as usize, Arc::downgrade(&i)))
 				.collect(),
-		))
+		)))
 	}
 }
 
@@ -116,14 +112,6 @@ impl<T: Send + Sync + Sized> FromIterator<Arc<T>> for Registry<T> {
 enum MaybeLazy<T> {
 	Lazy(LazyLock<T>),
 	NonLazy(T),
-}
-impl<T: Clone> Clone for MaybeLazy<T> {
-	fn clone(&self) -> Self {
-		match self {
-			MaybeLazy::Lazy(lazy_lock) => Self::NonLazy(lazy_lock.deref().clone()),
-			MaybeLazy::NonLazy(v) => Self::NonLazy(v.clone()),
-		}
-	}
 }
 impl<T> Deref for MaybeLazy<T> {
 	type Target = T;
