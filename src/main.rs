d14e03af918d2074e00fa78b2d73ea84e62f1a96
Nova <technobaboo@gmail.com>
2026-07-29 18:46:50 -0700

    revert: bevy_dylib

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,6 @@ mod openxr_helpers;
 mod query;
 mod session;
 
-#[allow(clippy::single_component_path_imports, unused_imports)]
-#[cfg(debug_assertions)]
-use bevy_dylib;
-
 use bevy::{
 	MinimalPlugins,
 	a11y::AccessibilityPlugin,
